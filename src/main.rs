0b5a04b482960d73ca4b5c24293a8c8f5d15f50b
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-02-06 12:38:55 -0500

    Change initial path for wordlist to allow MacOS binary to find the wordlists

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate sciter;
 mod load_handler;
 mod passgen;
 
+use std::env;
+use std::path::PathBuf;
+
 use sciter::Value;
 
 struct EventHandler {}
@@ -26,12 +29,24 @@ impl EventHandler {
         }
     }
 
+    fn get_resource_path(&self, resource_path: sciter::Value) -> String {
+        let res_path = resource_path.as_string().unwrap();
+        match env::current_exe() {
+            Ok(mut exe_path) => {
+                exe_path.pop();
+                exe_path.join(res_path).to_string_lossy().into_owned()
+            },
+            Err(_) => PathBuf::from(res_path).to_string_lossy().into_owned()
+        }
+    }
+
 }
 
 impl sciter::EventHandler for EventHandler {
 
     dispatch_script_call! {
         fn generate_password(Value);
+        fn get_resource_path(Value);
     }
 
 }
@@ -45,15 +60,15 @@ fn main() {
 
     let resources = include_bytes!("resources.rc");
     let handler = load_handler::LoadHandler::new(resources);
+    let event_handler = EventHandler{};
 
     let mut frame = sciter::window::Builder::main_window()
         .with_size((500, 275))
         .create();
 
     frame.sciter_handler(handler);
+    frame.event_handler(event_handler);
     frame.load_file("this://app/html/index.htm");
 
-    let event_handler = EventHandler{};
-    frame.event_handler(event_handler);
     frame.run_app();
 }

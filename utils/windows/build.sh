de3b2b28aa6e1968a6eadb5b75a062101832ed3c
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-02-06 23:47:25 -0500

    Add Windows build and package script

---
+++ utils/windows/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$(cygpath -m $SCRIPT_DIR)"
+$SCRIPT_DIR/build/packfolder.exe $SCRIPT_DIR/../../src/resources $SCRIPT_DIR/../../src/resources.rc -binary
+cargo build --release
+if [ ! -f "$SCRIPT_DIR/../../target/release/sciter.dll" ]; then
+	cp $SCRIPT_DIR/package/sciter.dll $SCRIPT_DIR/../../target/release
+fi
+$SCRIPT_DIR/../../target/release/pice.exe

e9fde3705e59cfcd73a8644c46600bb0f9e9ffb0
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2021-03-04 12:26:31 -0500

    style: run cargo fmt

--- build.rs
+++ build.rs
@@ -1,18 +1,20 @@
-use std::process::Command;
 use std::env;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     Command::new("x86_64-w64-mingw32-windres")
         .args(&["src/icon.rc"])
         .arg(&format!("{}/icon.o", out_dir))
-        .status().unwrap();
-    
+        .status()
+        .unwrap();
+
     Command::new("x86_64-w64-mingw32-gcc-ar")
         .args(&["crus", "libicon.a", "icon.o"])
         .current_dir(&Path::new(&out_dir))
-        .status().unwrap();
+        .status()
+        .unwrap();
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=icon");
